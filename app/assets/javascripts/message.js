1948ce8a58e6a184b44dd696bc48a4fb05e61373
woody-yoshihiro <woody19810930@gmail.com>
2020-03-16 16:03:00 +0900

    AT-update-function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,8 @@
 $(function(){
-  function buildHTML(message){
-    if ( message.image ) {
+  function buildHTML(message) {
+    if ( message.content && message.image ) {
       var html =
-        `<div class="message">
+        `<div class="message" data-message-id="${message.id}">
           <div class="message__info">
             <p class="message__info__name">
               ${message.user_name}
@@ -19,9 +19,27 @@ $(function(){
           <img src=${message.image} >
         </div>`
       return html;
-    } else {
-      var html =
-        `<div class="message">
+    } else if (message.content) {
+        var html =
+          `<div class="message" data-message-id="${message.id}">
+            <div class="message__info">
+              <p class="message__info__name">
+                ${message.user_name}
+              </p>
+              <p class="message__info__date">
+                ${message.created_at}
+              </p>
+            </div>
+            <div class="message__text">
+              <p class="message__text__content">
+                ${message.content}
+              </p>
+            </div>
+          </div>`
+        return html;
+    } else if (message.image) {
+        var html =
+        `<div class="message" data-message-id="${message.id}">
           <div class="message__info">
             <p class="message__info__name">
               ${message.user_name}
@@ -30,14 +48,10 @@ $(function(){
               ${message.created_at}
             </p>
           </div>
-          <div class="message__text">
-            <p class="message__text__content">
-              ${message.content}
-            </p>
-          </div>
+          <img src=${message.image} >
         </div>`
       return html;
-    };
+    }
   }
 $('#new_message').on('submit', function(e){
   e.preventDefault();
@@ -64,4 +78,29 @@ $('#new_message').on('submit', function(e){
       $('.submit_btn').prop('disabled', false);
     });
 })
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      datatype: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
